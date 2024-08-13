447f67d7a478de6dccfe21977bf5357bd17303c4
sergeilogachev <chizzplay@icloud.com>
2024-08-13 20:19:18 +0300

    add styles

--- Section2/01-starting-project/src/app/header.component.ts
+++ Section2/01-starting-project/src/app/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-header',
     standalone: true,
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export  class HeaderComponent {}
