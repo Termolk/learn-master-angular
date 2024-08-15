ee60b50ca71132698bde9c6d51e3386c6210aaba
sergeilogachev <chizzplay@icloud.com>
2024-08-15 20:31:59 +0300

    working with objects

--- Section2/01-starting-project/src/app/user/user.component.ts
+++ Section2/01-starting-project/src/app/user/user.component.ts
@@ -1,22 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
 
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
+
   get imagePath() {
-    return `assets/users/${this.avatar}`
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
